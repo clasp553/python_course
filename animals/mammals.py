10b9ec6d97a4dad3c9d12b3efc6f0754cf963f15
clasp553 <100148825+clasp553@users.noreply.github.com>
2022-02-23 19:11:46 +0100

    Delete animals directory

--- animals/mammals.py
+++
@@ -1,8 +0,0 @@
-class Mammals:
-    def __init__(self):
-        self.members = ["Tiger", "Elefant", "Wild Cat"]
-        
-    def printMembers(self):
-        print("Printing members of the Mammals class")
-        for member in self.members:
-            print("\t%s " % member)
